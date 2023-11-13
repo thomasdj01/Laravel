0f69564f06694314f9772760a41021f8a9fb8103
thomas schoemaker <thomasdj100@gmail.com>
2023-11-13 13:57:34 +0100

    Dinges toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RollercoasterController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +18,19 @@ use App\Http\Controllers\RollercoasterController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
 Route::get('/Rollercoaster', [RollercoasterController::class,'index']);
 
 Route::resource('Rollercoaster', RollercoasterController::class);
+
+require __DIR__.'/auth.php';
